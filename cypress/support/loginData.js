53af4829ed0a2fb1d413e46097d806994afbe075
gabrielaf0498 <145929028+gabrielaf0498@users.noreply.github.com>
2023-09-25 06:35:27 -0300

    Update loginData.js

--- cypress/support/loginData.js
+++ cypress/support/loginData.js
@@ -1,6 +1,6 @@
 const loginData = {
-    nome:'tsvini111@gmail.com',
-    senha:'iM3HxRWSy88ACJ7',
+    nome:'tsvini17@gmail.com',
+    senha:'Tijolo10',
     botaoEntrarOuCadastrar:'[data-testid="stereo-login_menu-unsigned_user_state-container"] > [data-testid="link"]',
     Botaologin:'#login-box-form-continue',  
     site:'https://www.magazineluiza.com.br',
